efffcd40e99f751bb59a84c9103c120e39c0e57d
Ëssa Mamdani <mamdani.info@gmail.com>
2024-09-05 05:05:00 +0500

    First Init

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,27 @@
-// src/page.tsx
-
 import dynamic from "next/dynamic";
 import { useMemo } from "react";
-
-export default async function Page() {
+export default function Page() {
     const Map = useMemo(() => dynamic(
         () => import('@/components/Map'),
         {
             loading: () => <div className="flex justify-center font-sans text-2xl items-center font-bold h-[100vh]"><p>Loading...</p></div>,
             ssr: false
         }
-    ), [])
+    ), []);
 
     return (
-        <Map />
+        <>
+            <Map />
+            <footer className="bg-gray-800 text-white py-4 mt-8">
+                <div className="container mx-auto text-center">
+                    <p>Powered by <a href="https://www.essamamdani.com" className="underline">Essa Mamdani</a></p>
+                    <div className="mt-2">
+                        <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fessamamdani%2Fgeojson&project-name=geojson&repo-name=geojson" className="inline-block">
+                            <img src="https://vercel.com/button" alt="Deploy with Vercel" />
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
